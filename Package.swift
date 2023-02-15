7b7e0972d59e219cbc45f9d7e962ce2c8af96115
Zaheer <zaheer-dvt@paystack.com>
2023-02-15 20:50:10 +0200

    Fixed package after renaming

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-  name: "fantastic-chainsaw",
+  name: "upgraded-chainsaw",
   platforms: [
     .macOS(.v10_15), .iOS(.v14)
   ],
   products: [
     .library(
-      name: "fantastic-chainsaw",
-      targets: ["fantastic-chainsaw"]),
+      name: "chainsaw",
+      targets: ["chainsaw"]),
   ],
   targets: [
     .binaryTarget(
-      name: "fantastic-chainsaw",
-      url: "https://github.com/Zamo22/fantastic-chainsaw/releases/download/0.0.3/fantastic-chainsaw.xcframework.zip",
-      checksum: "b0c6e356cf1b68db8e35c43ab98218e59f2d91583313ba03fb0f62f1a57c21c5"
+      name: "chainsaw",
+      url: "https://github.com/Zamo22/fantastic-chainsaw/releases/download/0.0.6/chainsaw.xcframework.zip",
+      checksum: "e1d03c3a65a975390300087b19be95ca9b6fbc03b809f9f0a2bee9ae1bf40d76"
     )
   ]
 )
